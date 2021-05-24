680500a94800122079daa21b58a90c1356430131
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2021-05-24 01:09:58 -0300

    learn
    Signed-off-by: Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,14 +1,56 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text, StyleSheet, SafeAreaView, TextInput, Platform, TouchableOpacity } from 'react-native';
 
 
 const Home = () => {
   return (
-    <View style={{flex: 1, backgroundColor: '#fff', justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Hello Ignite</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Hello Ignite</Text>
+
+      <TextInput style={styles.input} placeholder='New Skill' placeholderTextColor='#c3c3c3' />
+
+      <TouchableOpacity style={styles.button} activeOpacity={0.75}>
+        <Text style={styles.buttonText}>Add</Text>
+      </TouchableOpacity>
+
+      <Text style={[styles.title, { marginTop: 50 }]}>My skills</Text>   
+      {/* Da pra reaproveitar estilos de outros componentes e incrementar com algo que falta passando no array */}
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121015',
+    paddingHorizontal: 30,
+  },
+  title: {
+    color: '#FFF',
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  input: {
+    backgroundColor: '#1f1e24',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    color: '#fff',
+    marginTop: 30,
+    borderRadius: 7
+  },
+  button:{
+    backgroundColor: '#a370f7',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 15
+  },
+  buttonText:{
+    color: '#FFF',
+    fontSize: 17,
+    fontWeight: 'bold'
+  }
+})
 
 
 
