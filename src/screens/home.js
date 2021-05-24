dfdf5c005ceed92e7655090689df57820dc56a1e
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2021-05-24 00:36:01 -0300

    learn
    Signed-off-by: Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>

---
+++ src/screens/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+const Home = () => {
+  return (
+    <View style={{flex: 1, backgroundColor: '#fff', justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Hello Ignite</Text>
+    </View>
+  )
+}
+
+
+
+export default Home
